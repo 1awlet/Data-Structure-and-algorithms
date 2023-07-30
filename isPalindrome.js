8fc955a080a136371818dd16632424dea3fd3e42
Gele Hasan <111875403+1awlet@users.noreply.github.com>
2023-07-30 02:56:16 +0200

    Optimal solution, i used regular expression and  two-pointer technique

--- isPalindrome.js
+++ isPalindrome.js
@@ -4,23 +4,23 @@ Given an integer x, return true if x is a
 palindrome and false otherwise.
 */
 
-var isPalindrome = function(x) {
-    let palindrom="";
-   let xString= x.toString();
- 
-     for(let i=xString.length-1; i>=0; i--){
-         // console.log(xString[i])
-         palindrom +=xString[i];
-     }
-   
-     if(palindrom === xString ){
-         return true
-     }else{
+let isPalindrome = (word)=>{
+   word = word = word.replace(/[^A-Za-z0-9 ]/g, "");
+    word = word.replace(/\s/g, "").toLowerCase();
+
+    let p1=0;
+    let p2=word.length-1;
+
+  while(p1< p2){
+  
+     if(word[p1] != word[p2]){
         return false
+         
      }
-     
-     
-     
-     
-     
- };
+        p1++;
+        p2--;
+    }
+    
+   
+    return true
+}
