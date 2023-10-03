31362e2a51c2e88b7bb2c94052dbd53f430bf3da
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-10-03 16:45:30 +0200

    attempt to reverse the linkd listleft and right portion

--- ReverseLinkedListII.js
+++ ReverseLinkedListII.js
@@ -1,12 +1,59 @@
-/*
-Leet code explanation: Given the head of a singly linked list and two integers left and right where left <= right,
-reverse the nodes of the list from position left to position right, and return the reversed list.
-*/
+// Define a Node class to represent individual nodes in the linked list
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
+// Define a LinkedList class to represent the linked list itself
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
+  // Add a new node to the end of the linked list
+  append(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
 
-//Finding left and right
-findLeftAndRight(left, right) {
+  // Display the linked list
+  display() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+
+  // Reverse the linked list
+ reverse(){
+    let prev=null; 
+    let current=this.head;
+    let next= null;
+
+    while(current){
+        next= current.next;
+    
+        current.next=prev;
+        prev=current;
+
+        current=next;
+    }
+    this.head=prev;
+     
+ }
+ 
+ findLeftAndRight(left, right) {
   let leftPrev = null;
   let rightNext = null;
   let current = this.head;
@@ -33,8 +80,47 @@ findLeftAndRight(left, right) {
     prev = current.data;
     current = current.next;
   }
+  current=this.head;
+  let prevs;
+  let nexts=null;
+  
+  while(current){
+        
+    
+      while(current.data >= left && current.data <= right){
+       console.log(current.data)
+       nexts= current.next;
+       prevs=current;
+       current=nexts;
+      }
+      console.log(prevs)
+      
+    current = current.next;
+  }
+  
+  
 
   console.log("Left Previous:", leftPrev);
   console.log("Right Next:", rightNext);
 }
+
+}
+
+// Create a new linked list and add elements to it
+const myList = new LinkedList();
+myList.append(1);
+myList.append(2);
+myList.append(3);
+myList.append(4);
+myList.append(5);
+myList.append(6);
+myList.append(7);
+
+// Display the original linked list
+console.log("Original Linked List:");
+
+// Reverse the linked list
+myList.findLeftAndRight(3,5)
+
+
 
