7f2b6ea24549f93d383cd51eeca8d3a3381d0f97
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-10-25 23:33:38 +0200

    finding max element using recursive

--- findMaxRecursive.js
+++ findMaxRecursive.js
@@ -1,1 +1,14 @@
-fi
+function findMaximum(A, N) {
+    if (N === 1) {
+        return A[0];
+    }
+    
+    const maxPrevious = findMaximum(A, N - 1);
+    
+    return maxPrevious < A[N - 1] ? A[N - 1] : maxPrevious;
+}
+
+const array = [4, 8, 2, 10, 7];
+const length = array.length;
+const maximum = findMaximum(array, length);
+console.log("Maximum element:", maximum);
