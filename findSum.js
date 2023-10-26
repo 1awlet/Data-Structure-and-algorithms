9c303b2a62adfc5ba19a9405fa5c6f754f3bbe17
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-10-26 17:03:48 +0200

    summing the element using recursion

--- findSum.js
+++ findSum.js
@@ -1,1 +1,11 @@
 
+function F2(A, N) {
+    if (N === 0) {
+        return 0;
+    }
+    return F2(A, N - 1) + A[N - 1];
+}
+
+let arr = [1, 5, 7];
+let result = F2(arr, arr.length);
+console.log(result);
