a8ff613ff790bc2a693ce1df8e040660b19633bc
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-10-26 02:13:50 +0200

    Finds element if it exists in the array using recursion

--- findElement.js
+++ findElement.js
@@ -1,1 +1,21 @@
+function findNum(A, n, chosenNum){
+ 
+    if(A[n-1] ==chosenNum){
+   
+        return true
+    }
+    
+          
+    if(n== 0){
+        return false
+    }
+
+    
+ return   findNum(A,n-1, chosenNum)
+}
+
+let arr=[3,4,5]
+console.log(findNum(arr,arr.length, 4))
+
+
 
