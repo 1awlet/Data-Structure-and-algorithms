a0e4d1ff42764dab7a16aabd4bc5cbd52673ecfa
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-11-06 15:19:22 +0100

    pseudocode for  BubbleSort algorthim

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,1 +1,18 @@
 
+function BubbleSort(A,N)
+ swapped=true
+
+while(swapped) do
+ swapped= false
+
+for 0 <= i <N-1 do
+ if (A[i] > A[i+1]) then
+  swap(A[i], A[i+1)
+  swapped=true
+end if
+
+end for 
+N= N-1
+end while 
+return A
+end function
