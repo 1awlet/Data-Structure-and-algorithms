d7c12612c0cab30e59f1cba18b44cd657a867470
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-11-08 22:26:02 +0100

    turned the pseudocode to javascript .

--- QuickSort.js
+++ QuickSort.js
@@ -1,16 +1,16 @@
 # Pseudocode for the Quick Sort algorithm
-function quickSort(A, low, high)
-    if low < high then
-    
-        # Partition the array A and get the pivot's index
-        p <-- partition(A, low, high)
+function quickSort(A, low, high) {
+    if (low < high) {
+        // Partition the array and get the pivot's index
+        const p = partition(A, low, high);
         
-        # Recursively sort the elements to the left of the pivot
-        quickSort(A, low, p - 1)
+        // Recursively sort the elements to the left of the pivot
+        quickSort(A, low, p - 1);
         
-        # Recursively sort the elements to the right of the pivot
-        quickSort(A, p + 1, high)
-    
-    end if
-end function
+        // Recursively sort the elements to the right of the pivot
+        quickSort(A, p + 1, high);
+    }
+}
+
+
 
