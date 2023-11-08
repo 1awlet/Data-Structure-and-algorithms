535dbfe6fe7fdf500178ce765ca2a55cce19bfc1
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-11-08 22:27:02 +0100

    finilized the algorthim and implemented partition function

--- QuickSort.js
+++ QuickSort.js
@@ -14,3 +14,26 @@ function quickSort(A, low, high) {
 
 
 
+function partition(A, low, high) {
+    // Choose the pivot (in this example, we'll choose the rightmost element)
+    const pivot = A[high];
+    let i = low - 1; // Index of the smaller element
+    
+    for (let j = low; j < high; j++) {
+        // If the current element is smaller than or equal to the pivot
+        if (A[j] <= pivot) {
+            i++; // Increment the index of the smaller element
+            // Swap A[i] and A[j]
+            const temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+    }
+    
+    // Swap A[i + 1] and the pivot
+    const temp = A[i + 1];
+    A[i + 1] = A[high];
+    A[high] = temp;
+    
+    return i + 1; // Return the index of the pivot
+}
