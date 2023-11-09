a36a3dc26af816b2b1cacca4c66bd6003f343219
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-11-09 23:28:04 +0100

    provided comment Fibonacci

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,16 +1,14 @@
-
-
-
-function fibinaci(n){
-    if(n<= 1){
-           return n; 
+// Define a function called 'fibonacci' that calculates the nth Fibonacci number.
+function fibonacci(n) {
+    // Base case: If n is 0 or 1, return n itself (Fibonacci sequence starts with 0 and 1).
+    if (n <= 1) {
+        return n;
     }
 
-
-    return (fibinaci(n-1) + fibinaci(n-2))
+    // Recursive case: For n greater than 1, calculate the nth Fibonacci number
+    // by recursively summing the (n-1)th and (n-2)th Fibonacci numbers.
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-
-console.log(fibinaci(23))
-
-
+// Call the 'fibonacci' function with the argument 23 to calculate the 23rd Fibonacci number.
+console.log(fibonacci(23)); // This will output the 23rd Fibonacci number.
