5610fe4c94d6e91971572abd8f0d0e94e94e1cb4
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-12-04 18:09:43 +0100

    Created pseudocode for counting sort

--- CountingSort.js
+++ CountingSort.js
@@ -1,1 +1,21 @@
 
+Function counting-sort(A,K)
+C --> new array(k+1) of zeros
+R --> new array(length(A)) of zeros
+
+Post --> 0
+
+For 0<=j <length(A) do
+c[A[j]] +1
+End for
+
+For 0 < i < (k+1) do
+For pos <= r < post+c[i] do
+R[r]=i
+End for
+Post = r
+End for
+
+Return R
+
+End function
