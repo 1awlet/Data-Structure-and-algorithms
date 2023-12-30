518214e37a3865d397063df8268096989eedce98
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-12-30 16:39:32 +0100

    implenmented string checking

--- vigenereCipher.js
+++ vigenereCipher.js
@@ -1,8 +1,34 @@
+
+
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const mapAlphabetsToNumbers = (key,plainText)=>{
+    const mappedHash = {};
+    let secretKeyCount= 0;
+    
+    for(let letter in plainText){
+        const letterNumber = alphabet.indexOf(plainText[letter])
+       
+       if(key.length <= secretKeyCount){
+           secretKeyCount=0;
+       }
+       
+       console.log(key[secretKeyCount])
+       // console.log(plainText[letter] , " corresponds to ", //letterNumber)
+        
+        
+        secretKeyCount++;
+    }
+}
 
 const decryptText = (key, plainText)=>{
     
     if(/^[A-Z]+$/.test(plainText)){
-        console.log("true");
-    } 
+        mapAlphabetsToNumbers (key,plainText)
+    }else{
+        console.log("false")
+    }
 }
+
+
+decryptText("BANANA", "ILOVEPEANUTS")
