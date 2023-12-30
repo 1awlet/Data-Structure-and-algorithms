e4d9fd7d67ee322b6decb7d3e6701984af9b318a
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2023-12-30 17:10:07 +0100

    implemented converting secret key and plain text to ciper text

--- vigenereCipher.js
+++ vigenereCipher.js
@@ -2,32 +2,54 @@
 
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+
+
 const mapAlphabetsToNumbers = (key,plainText)=>{
-    const mappedHash = {};
+    let decryptedMessage ="";
     let secretKeyCount= 0;
     
     for(let letter in plainText){
-        const letterNumber = alphabet.indexOf(plainText[letter])
-       
-       if(key.length <= secretKeyCount){
+        const textLetterNumber = alphabet.indexOf(plainText[letter])
+          
+          if(key.length <= secretKeyCount){
            secretKeyCount=0;
+            }
+    
+        const keyLetterToNumber = alphabet.indexOf(key[secretKeyCount]);
+       
+       
+    
+       if(textLetterNumber + keyLetterToNumber <= 25){
+          
+           const ciperAdding = textLetterNumber + keyLetterToNumber;
+
+          const decryptedLetter = alphabet[ciperAdding]
+       
+         decryptedMessage += decryptedLetter;
+          console.log(decryptedMessage)
+       }else{
+          const ciperAdding = (textLetterNumber + keyLetterToNumber) % 26
+           
        }
        
-       console.log(key[secretKeyCount])
-       // console.log(plainText[letter] , " corresponds to ", //letterNumber)
-        
-        
+    
         secretKeyCount++;
     }
+    
+    
+   
+
 }
 
 const decryptText = (key, plainText)=>{
-    
-    if(/^[A-Z]+$/.test(plainText)){
-        mapAlphabetsToNumbers (key,plainText)
-    }else{
-        console.log("false")
+    if(/^[A-Z]+$/.test(plainText) === false){
+        console.log("please change the text to capital letter")
+        return
     }
+    
+    const mappedTextToKey= mapAlphabetsToNumbers (key,plainText);
+    
+
 }
 
 
