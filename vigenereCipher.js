65b7bfe105b661e7d6223170dc785dbdc77f249a
Gele <gellehussen@gmail.com>
2024-01-01 12:28:04 +0100

    hided vigenere cipher for midterm purposes

--- vigenereCipher.js
+++ vigenereCipher.js
@@ -1,3 +1,43 @@
-/** 
-Hidden for mid term purposes
-*/
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const encryptMessage = (key, plainText) => {
+
+    let encryptedMessage = "";
+    let secretKeyCount = 0;
+
+    for (let i = 0; i < plainText.length; i++) {
+        // key loops which makes it always at least be equal or it can be greater
+        if (secretKeyCount >= key.length) {
+            secretKeyCount = 0;
+        }
+        // assigning each letter to a number
+        const textLetterNumber = alphabet.indexOf(plainText[i]);
+        const keyLetterToNumber = alphabet.indexOf(key[secretKeyCount]);
+        
+        let cipherNumber = (textLetterNumber + keyLetterToNumber) % 26;
+       
+        
+        encryptedMessage += alphabet[cipherNumber];
+        secretKeyCount++;
+    }
+    
+    return encryptedMessage;
+}
+
+const encryptText = (key, plainText) => {
+        //Turning massage to capital letters
+        plainText= plainText.toUpperCase();
+        key = key.toUpperCase();
+    
+ // Check if the text contains only letters A-Z
+    if (!/^[A-Z]+$/.test(plainText) || !/^[A-Z]+$/.test(key)) {
+        console.log("Please only use alphabets from A-Z for both the plaintext and the key.");
+        return;
+    }
+    
+    const encryptedText = encryptMessage(key, plainText);
+    console.log(encryptedText);
+    return encryptedText;
+}
+
+encryptText("”THISISANEXAMPLEKEYINCOMPUTERSECURITYEXAM”", "Gele");
