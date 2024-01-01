f342f1ccae551d525d9e86635d7ba12bda1b4952
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-01-01 11:59:25 +0100

    intialized decryption letters testing

--- decryptText.js
+++ decryptText.js
@@ -1,1 +1,14 @@
 
+const decryptText = (key, cipherText) => {
+    cipherText = cipherText.toUpperCase();
+    key = key.toUpperCase();
+    
+    if (!/^[A-Z]+$/.test(cipherText) || !/^[A-Z]+$/.test(key)) {
+        console.log("Please only use alphabets from A-Z for both the ciphertext and the key.");
+        return;
+    }
+    
+    const decryptedText = decryptMessage(key, cipherText);
+    console.log(decryptedText);
+    return decryptedText;
+}
