556f6e9729f0ebce8eb7f43cd42af3cf91edcfcf
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-01-26 23:24:55 +0100

    adding enqueue and dequeue algorhtim

--- Queue.js
+++ Queue.js
@@ -1,1 +1,21 @@
+class Queue {
+    constructor() {
+        this.items = [];
+    }
 
+    // Adds an element to the back of the queue
+    enqueue(element) {
+        this.items.push(element);
+    }
+
+    // Removes and returns the element at the front of the queue
+    dequeue() {
+        if (this.isEmpty()) {
+            console.log("Queue is empty!");
+            return null;
+        }
+        return this.items.shift();
+    }
+
+
+}
