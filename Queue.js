a3334ba12cefdf9ed9ac3f3b032f715bbd4c62c3
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-01-26 23:26:13 +0100

    added more functionality algorthim to the queue

--- Queue.js
+++ Queue.js
@@ -18,4 +18,23 @@ class Queue {
     }
 
 
+        // Returns the element at the front of the queue without removing it
+    front() {
+        if (this.isEmpty()) {
+            console.log("Queue is empty!");
+            return null;
+        }
+        return this.items[0];
+    }
+
+    // Checks if the queue is empty
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    // Returns the size of the queue
+    size() {
+        return this.items.length;
+    }
+
 }
