6c804d70c657ab5b8029dfebf6b766d9e0a1ae18
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-01 20:29:00 +0100

    defined variables we will be using

--- knapsack.js
+++ knapsack.js
@@ -1,4 +1,9 @@
 function knapsack(values, weights, capacity) {
+  const n = values.length;
+    // Creating a 2D array to store the maximum value that can be obtained
+    // using the first i items and capacity j
+    const dp = Array.from({ length: n + 1 }, () => Array(capacity + 1).fill(0));
+
 
 
 
