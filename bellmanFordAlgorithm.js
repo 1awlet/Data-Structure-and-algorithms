f13ebeecd890df99dadd1f5a63a1745ac1a47f3b
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-03 20:22:06 +0100

    initialized variables

--- bellmanFordAlgorithm.js
+++ bellmanFordAlgorithm.js
@@ -1,1 +1,8 @@
+function bellmanFord(graph, V, E, src) {
+    // Initialized distance of all vertices as infinite.
+    const dist = new Array(V).fill(Infinity);
+    // Distance to the source is always 0
+    dist[src] = 0;
 
+
+}
