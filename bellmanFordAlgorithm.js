a723f0f65fdc06e08da7aef097b9105959cb5465
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-03 20:22:55 +0100

    short path from src

--- bellmanFordAlgorithm.js
+++ bellmanFordAlgorithm.js
@@ -4,5 +4,16 @@ function bellmanFord(graph, V, E, src) {
     // Distance to the source is always 0
     dist[src] = 0;
 
+     //  A simple shortest path from src
+    for (let i = 1; i <= V - 1; i++) {
+        for (let j = 0; j < E; j++) {
+            let u = graph[j][0];
+            let v = graph[j][1];
+            let weight = graph[j][2];
+            if (dist[u] !== Infinity && dist[u] + weight < dist[v]) {
+                dist[v] = dist[u] + weight;
+            }
+        }
+    }
 
 }
