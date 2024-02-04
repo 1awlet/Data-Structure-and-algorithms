b6c7f23bcdb3d50c01f31f3bb69680022dae7655
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-04 21:03:48 +0100

    implemented enqueue logic

--- dijkstraAlgorithm.js
+++ dijkstraAlgorithm.js
@@ -3,5 +3,22 @@ class PriorityQueue {
         this.collection = [];
     }
 
+     enqueue(element) {
+        if (this.isEmpty()) {
+            this.collection.push(element);
+        } else {
+            let added = false;
+            for (let i = 0; i < this.collection.length; i++) {
+                if (element[1] < this.collection[i][1]) { // checking priorities
+                    this.collection.splice(i, 0, element);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added) {
+                this.collection.push(element);
+            }
+        }
+    }
 
 }
