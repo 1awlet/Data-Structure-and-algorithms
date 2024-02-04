0e4782aa1ebc7221e3b3750a3bd9b62b6df065f8
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-04 21:05:32 +0100

    test cases

--- dijkstraAlgorithm.js
+++ dijkstraAlgorithm.js
@@ -62,3 +62,10 @@ function dijkstraAlgorithm(graph, startVertex) {
 
     return { distances, prev };
 }
+const graph = {
+    A: {B: 1, C: 4},
+    B: {A: 1, C: 2, D: 5},
+    C: {A: 4, B: 2, D: 1},
+    D: {B: 5, C: 1}
+};
+
