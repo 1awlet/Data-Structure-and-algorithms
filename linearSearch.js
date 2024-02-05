999a3bb3a850dcd73d93ab4467765d5874511564
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-05 21:39:22 +0100

    implemented function that runs through the array

--- linearSearch.js
+++ linearSearch.js
@@ -1,1 +1,9 @@
-
+function linearSearch(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+          // Return the index of the target element
+            return i; 
+        }
+    }
+    return -1; 
+}
