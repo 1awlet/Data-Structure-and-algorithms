bd84d02645dd9bb8d29df09af7540d32eeea781f
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-06 22:49:20 +0100

    added BinaryTree

--- treesPart1.js
+++ treesPart1.js
@@ -5,3 +5,43 @@ class TreeNode {
         this.right = null;
     }
 }
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+  
+    insert(value) {
+        const newNode = new TreeNode(value);
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    // In order traversal function
+    inOrderTraverse(node, callback) {
+        if (node !== null) {
+            this.inOrderTraverse(node.left, callback);
+            callback(node.value);
+            this.inOrderTraverse(node.right, callback);
+        }
+    }
+}
