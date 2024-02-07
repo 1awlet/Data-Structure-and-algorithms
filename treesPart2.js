9493a11e1ec3b61ee2e9fe7224bd48b0e88ac561
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-07 22:58:42 +0100

    added insert algorthim

--- treesPart2.js
+++ treesPart2.js
@@ -9,3 +9,35 @@ class TreeNode {
 
 
       
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    // Insert a node in the BST
+    insert(value) {
+        const newNode = new TreeNode(value);
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this._insertNode(this.root, newNode);
+        }
+    }
+
+    _insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this._insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this._insertNode(node.right, newNode);
+            }
+        }
+    }
+
+}
