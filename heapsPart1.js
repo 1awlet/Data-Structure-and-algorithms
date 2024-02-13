a8161b7bbf2329af82fda7a6f63a2dcbf976c01a
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-13 21:19:20 +0100

    implemented parent function and peak

--- heapsPart1.js
+++ heapsPart1.js
@@ -10,4 +10,16 @@ class MinHeap {
   getRightChildIndex(parentIndex) {
     return 2 * parentIndex + 2;
   }
+
+
+  getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+
+  swap(index1, index2) {
+    const temp = this.heap[index1];
+    this.heap[index1] = this.heap[index2];
+    this.heap[index2] = temp;
+  }
+  
 }
