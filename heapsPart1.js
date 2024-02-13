be2a37a3d16d4ab3c47b89acbc5a138cedc96b45
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-13 21:18:52 +0100

    implemented left and right child function

--- heapsPart1.js
+++ heapsPart1.js
@@ -3,4 +3,11 @@ class MinHeap {
     this.heap = [];
   }
 
+  getLeftChildIndex(parentIndex) {
+    return 2 * parentIndex + 1;
+  }
+
+  getRightChildIndex(parentIndex) {
+    return 2 * parentIndex + 2;
+  }
 }
