253114e441a824739bb6588b590784a62fbac263
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-19 23:12:06 +0100

    code fir showing connections

--- graphsPart1.js
+++ graphsPart1.js
@@ -15,4 +15,18 @@ class Graph {
     this.adjacencyList[v1].push(v2);
     this.adjacencyList[v2].push(v1); // Omit this line for a directed graph
   }
+
+
+
+   showConnections() {
+    const keys = Object.keys(this.adjacencyList);
+    for (let vertex of keys) {
+      let connections = "";
+      let vertexList = this.adjacencyList[vertex];
+      for (let j = 0; j < vertexList.length; j++) {
+        connections += vertexList[j] + " ";
+      }
+      console.log(vertex + " --> " + connections);
+    }
+  }
 }
