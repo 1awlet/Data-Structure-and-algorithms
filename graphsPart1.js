55f8c532038da99b4ff09a6574c3e35576a3f0cb
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-19 23:11:31 +0100

    code for addubg vertxes and edge

--- graphsPart1.js
+++ graphsPart1.js
@@ -3,4 +3,16 @@ class Graph {
     this.adjacencyList = {};
   }
 
+  // Add vertex
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  // Add edge
+  addEdge(v1, v2) {
+    if (!this.adjacencyList[v1]) this.addVertex(v1);
+    if (!this.adjacencyList[v2]) this.addVertex(v2);
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1); // Omit this line for a directed graph
+  }
 }
