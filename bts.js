4720d10245b4ecff83c902cca660d154489deba0
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-24 23:45:47 +0100

    implemented insert operation

--- bts.js
+++ bts.js
@@ -12,4 +12,29 @@ class BinarySearchTree {
     this.root = null;
   }
 
+  insert(data) {
+    const newNode = new Node(data);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
 }
