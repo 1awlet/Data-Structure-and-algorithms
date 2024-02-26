e09ab736267253f6407e4b68e7993dcfccc579a6
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-26 22:49:18 +0100

    implemented the prismt logic

--- PrimsAlgorthim.js
+++ PrimsAlgorthim.js
@@ -1,1 +1,25 @@
+function primMST(graph) {
+  const vertices = graph.vertices();
+  const T = new Graph([vertices[0]], []);
 
+  while (T.vertices.length < graph.vertices.length) {
+    let edges = graph.edges.filter(e => 
+      T.vertices.includes(e.from) && !T.vertices.includes(e.to)
+    );
+
+    if (edges.length === 0) {
+      // No more edges to add, which means the mst is complete or the graph is not fully connected
+      break;
+    }
+
+
+    edges.sort((a, b) => a.weight - b.weight);
+    // Edge with the smallest weight
+    let newE = edges[0]; 
+
+    T.addVertex(newE.to);
+    T.addEdge(newE); 
+  }
+
+  return T;
+}
