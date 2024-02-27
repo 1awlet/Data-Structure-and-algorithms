8f11f1bbccd0979df62cfebb48a10a93d02d012f
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-27 23:11:01 +0100

    implemented the KRUSKAL_MST psedecode

--- Kruskal'sPsedecode.js
+++ Kruskal'sPsedecode.js
@@ -1,1 +1,13 @@
+function KRUSKAL_MST(G)
+vs = vertices(G)
+T = new Graph(vs, {})
+F = new DisjointSet()
+for v in vs do
+    MAKE-SET(F, v)
+L = sort(edges(G))
+for e in L do
+    if FIND(F, FROM(e)) ≠ FIND(F, TO(e))
+        addEdge(T, e)
+        UNION(F, FROM(e), TO(e))
+end function
 
