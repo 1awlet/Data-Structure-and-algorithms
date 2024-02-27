6df508a70de942f6594138b0fee182304de23473
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-27 23:13:36 +0100

    implemented the disjoint set

--- KruskalAlgorthim.js
+++ KruskalAlgorthim.js
@@ -1,1 +1,24 @@
+class DisjointSet {
+    constructor() {
+        this.parent = {};
+    }
+
+    makeSet(u) {
+        this.parent[u] = u;
+    }
+
+    find(u) {
+        if (this.parent[u] !== u) {
+            this.parent[u] = this.find(this.parent[u]);
+        }
+        return this.parent[u];
+    }
+
+    union(u, v) {
+        let uRoot = this.find(u);
+        let vRoot = this.find(v);
+        if (uRoot === vRoot) return;
+        this.parent[uRoot] = vRoot;
+    }
+}
 
