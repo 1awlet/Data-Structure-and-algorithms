f7a22e5df9e6d848c219627ce045322a428a32e0
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-28 23:50:25 +0100

    test cases

--- DijkstraUpdated.js
+++ DijkstraUpdated.js
@@ -77,3 +77,14 @@ function dijkstra(graph, startNode, endNode) {
   return distances;
 }
 
+
+
+
+const graph = {
+  A: {B: 1, C: 4},
+  B: {A: 1, C: 2, D: 5},
+  C: {A: 4, B: 2, D: 1},
+  D: {B: 5, C: 1}
+};
+
+console.log(dijkstra(graph, 'A', 'D'));
