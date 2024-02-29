01400e27cbb4dcd49b3850e871c0c85b9a17b314
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-29 23:09:36 +0100

    Implemented the logic for counting unique numbers

--- uniqueOccurrences.js
+++ uniqueOccurrences.js
@@ -1,1 +1,16 @@
+function uniqueOccurrences(arr) {
+    const countMap = {};
+        
+    arr.forEach(num => {
+        if (countMap[num]) {
+            countMap[num] += 1;
+        } else {
+            countMap[num] = 1;
+        }
+    });
 
+    const counts = Object.values(countMap);
+    const uniqueCounts = new Set(counts);
+
+    return counts.length === uniqueCounts.size;
+}
