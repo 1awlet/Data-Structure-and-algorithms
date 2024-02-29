56fec94f9cd5eedacf5029d8001fcffa79e7fd2c
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-02-29 23:11:49 +0100

    Provided comments

--- uniqueOccurrences.js
+++ uniqueOccurrences.js
@@ -1,6 +1,6 @@
 function uniqueOccurrences(arr) {
     const countMap = {};
-        
+     // Count occurrences of each number
     arr.forEach(num => {
         if (countMap[num]) {
             countMap[num] += 1;
@@ -11,6 +11,6 @@ function uniqueOccurrences(arr) {
 
     const counts = Object.values(countMap);
     const uniqueCounts = new Set(counts);
-
+    // Check if the length of counts array is the same as the uniqueCounts Set
     return counts.length === uniqueCounts.size;
 }
