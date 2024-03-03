4d6b201736630e2a38fd4b960d547fbf95788cd5
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-03-03 20:37:53 +0100

    implemented the logic

--- mergeIntervals.js
+++ mergeIntervals.js
@@ -1,1 +1,16 @@
 
+function mergeIntervals(intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    const mergedIntervals = [];
+    
+    intervals.forEach(interval => {
+        if (mergedIntervals.length === 0 || mergedIntervals[mergedIntervals.length - 1][1] < interval[0]) {
+            mergedIntervals.push(interval);
+        } else {
+            // There is an overlap, so we merge the current interval with the last interval of mergedIntervals.
+            mergedIntervals[mergedIntervals.length - 1][1] = Math.max(mergedIntervals[mergedIntervals.length - 1][1], interval[1]);
+        }
+    });
+    
+    return mergedIntervals;
+}
