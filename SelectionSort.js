14cc326625c9766d3a5d72f140eb1d63bfe67dca
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-03-08 21:40:02 +0100

    looping through the array and selecting min

--- SelectionSort.js
+++ SelectionSort.js
@@ -1,4 +1,17 @@
 function selectionSort(arr) {
     let n = arr.length;
 
+    
+    for (let i = 0; i < n - 1; i++) {
+        // Finding the smallest number in the subarray
+        let minPos = posMin(arr, i, n - 1);
+
+        // Swapping the elements
+        if (minPos !== i) { // Swap only if needed
+            let temp = arr[i];
+            arr[i] = arr[minPos];
+            arr[minPos] = temp;
+        }
+    }
+
 }
