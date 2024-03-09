970a8bc437209ce90b5d6ecae2dea3713b7fc75e
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-03-09 23:44:26 +0100

    Initialized counting sort function and counting each occurence of each digit

--- raddixSort.js
+++ raddixSort.js
@@ -1,1 +1,15 @@
+function countingSortForRadix(arr, exp) {
+  let output = new Array(arr.length);
+  let count = new Array(10).fill(0);
+  let i;
+
+   // Count occurrences of each digit
+  for (i = 0; i < arr.length; i++) {
+    count[Math.floor(arr[i] / exp) % 10]++;
+  }
+
+
+
+
+}
 
