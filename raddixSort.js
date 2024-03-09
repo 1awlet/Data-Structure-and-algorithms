881884765bb0b8efe1abd5540180bc70fc221e1a
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-03-09 23:44:52 +0100

    implemented algorthim for building the output array

--- raddixSort.js
+++ raddixSort.js
@@ -9,7 +9,15 @@ function countingSortForRadix(arr, exp) {
   }
 
 
+for (i = 1; i < 10; i++) {
+    count[i] += count[i - 1];
+  }
 
+  // Building the output array
+  for (i = arr.length - 1; i >= 0; i--) {
+    output[count[Math.floor(arr[i] / exp) % 10] - 1] = arr[i];
+    count[Math.floor(arr[i] / exp) % 10]--;
+  }
 
 }
 
