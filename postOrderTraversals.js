fe6504b428594b1a553341c27e83eb8390cbe08e
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-03-14 22:49:11 +0100

    implemented logic

--- postOrderTraversals.js
+++ postOrderTraversals.js
@@ -1,1 +1,8 @@
 
+function postOrderTraversal(node) {
+    if (node !== null) {
+        postOrderTraversal(node.left);  
+        postOrderTraversal(node.right);  
+        console.log(node.value);  
+    }
+}
