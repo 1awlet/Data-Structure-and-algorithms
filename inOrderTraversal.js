fa63fc0e0795637f5439c05f6d1df090a97116d6
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-03-14 22:47:11 +0100

    implemented for traversing root in middle

--- inOrderTraversal.js
+++ inOrderTraversal.js
@@ -1,1 +1,7 @@
-
+function inOrderTraversal(node) {
+    if (node !== null) {
+        inOrderTraversal(node.left);  
+        console.log(node.value);  
+        inOrderTraversal(node.right); 
+    }
+}
