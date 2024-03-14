32d40567856d1fb9eb36a2260a5c5fa4fa189414
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-03-14 22:46:15 +0100

    implemented the logic

--- postOrderTraversal.js
+++ postOrderTraversal.js
@@ -1,1 +1,8 @@
 
+function preOrderTraversal(node) {
+    if (node !== null) {
+        console.log(node.value); 
+        preOrderTraversal(node.left); 
+        preOrderTraversal(node.right);
+    }
+}
