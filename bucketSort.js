035efcf5478d95dbdd411732467ed1b563f5c121
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-03-15 23:20:36 +0100

    added algorthim for determining minimum and maximum

--- bucketSort.js
+++ bucketSort.js
@@ -3,5 +3,17 @@ function bucketSort(arr, bucketSize = 5) {
         return arr;
     }
 
+    // Determine minimum and maximum values in the input array
+    let minValue = arr[0];
+    let maxValue = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < minValue) {
+            minValue = arr[i];
+        } else if (arr[i] > maxValue) {
+            maxValue = arr[i];
+        }
+    }
+
+
 
 }
