c25c0a7ac215a3d57da4aa783ed0f35ec88d16b7
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-09-24 22:50:36 +0200

    helper function

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -19,3 +19,12 @@ void selectionSort(vector<int>& arr) {
         swap(arr[minIndex], arr[i]);
     }
 }
+
+
+
+void printArray(const vector<int>& arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
